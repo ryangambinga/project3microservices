c2006a47f80dd76e4287a2a5803f616343a60e61
ryangambinga <59413049+ryangambinga@users.noreply.github.com>
2022-10-03 02:17:20 +0200

    Added K8s and Updated Frontend

---
+++ deployment.sh
@@ -0,0 +1,9 @@
+kubectl delete deploy backend-feed
+kubectl delete deploy backend-user
+kubectl delete deploy frontend
+kubectl delete deploy reverseproxy
+
+kubectl apply -f reverseproxy.yaml
+kubectl apply -f frontend.yaml
+kubectl apply -f backend-feed-deployment.yaml
+kubectl apply -f backend-user-deployment.yaml
