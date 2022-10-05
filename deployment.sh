b6ca5755fce5ac2704f9f05b3b9852ed219a5c18
ryangambinga <59413049+ryangambinga@users.noreply.github.com>
2022-10-05 05:37:22 +0200

    update

--- deployment.sh
+++ deployment.sh
@@ -3,6 +3,7 @@ kubectl delete deploy backend-user
 kubectl delete deploy frontend
 kubectl delete deploy reverseproxy
 
+kubectl apply -f aws-creds-configmap.yaml
 kubectl apply -f reverseproxy.yaml
 kubectl apply -f frontend.yaml
 kubectl apply -f backend-feed-deployment.yaml
